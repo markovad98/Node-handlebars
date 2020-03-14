47ac6579d0d621ebe52d8d2378116a84f0a7def9
AlexeyOpenDev <alexey.markov@opendev.com>
2020-03-14 17:56:36 +0400

    add course-edit logic

--- models/Course.js
+++ models/Course.js
@@ -15,18 +15,43 @@ class Course {
     courses.push(this);
 
     return new Promise((res, rej) => {
-        fs.writeFile(
-            path.join(__dirname, '..', 'data', 'courses.json'),
-            JSON.stringify(courses),
-            err => {
-                if (err) {
-                    rej(err)
-                } else {
-                    res()
-                }
+      fs.writeFile(
+        path.join(__dirname, "..", "data", "courses.json"),
+        JSON.stringify(courses),
+        err => {
+          if (err) {
+            rej(err);
+          } else {
+            res();
+          }
+        }
+      );
+    });
+  }
+
+  static async getById(id) {
+    const courses = await Course.getAll();
+    return courses.find(course => course.id === id)
+  }
+
+  static async update(course) {
+    const courses = await Course.getAll();
+    const idx = courses.findIndex(item => item.id === course.id);
+    courses[idx] = course;
+
+    return new Promise((res, rej) => {
+      fs.writeFile(
+          path.join(__dirname, "..", "data", "courses.json"),
+          JSON.stringify(courses),
+          err => {
+            if (err) {
+              rej(err);
+            } else {
+              res();
             }
-        )
-    })
+          }
+      );
+    });
   }
 
   static getAll() {
--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,40 @@
-const { Router } = require('express');
-const Courses = require('../models/Course');
+const { Router } = require("express");
+const Courses = require("../models/Course");
 const router = Router();
 
 router.get("/", async (req, res) => {
-    const courses = await Courses.getAll();
-    res.render("courses", {
-        title: 'Courses',
-        isCourses: true,
-        courses,
+  const courses = await Courses.getAll();
+  res.render("courses", {
+    title: "Courses",
+    isCourses: true,
+    courses
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const course = await Courses.getById(req.params.id);
+  res.render("course", {
+    course,
+    layout: "empty",
+    title: `Курс: ${course.title}`
+  });
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const course = await Courses.getById(req.params.id);
+  if (!req.query.allow) {
+    res.redirect("/");
+  } else {
+    res.render("courseEdit", {
+      course,
+      title: `Редактировать курс ${course.title}`
     });
+  }
+});
+
+router.post("/edit", async (req, res) => {
+  await Courses.update(req.body);
+  res.redirect("/");
 });
 
 module.exports = router;
